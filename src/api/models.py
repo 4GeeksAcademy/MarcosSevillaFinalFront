7ffa8087bb2610e60d0954e36124a09a57facb50
Marcos Sevilla Miñana <marcos.sevilla@carset.es>
2025-01-25 11:57:48 +0000

    fix

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    phone = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.id} - {self.email}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        # Do not serialize the password, its a security breach
+        return {"id": self.id, 
+                "email": self.email,
+                "first_name": self.first_name,
+                "phone": self.phone}
